5b0633f819e82d6f82d4f09f188d88c34fe56c53
zhurainyk <zhurainyk@163.com>
2021-05-31 11:03:59 +0800

    generator详情

---
+++ Javascript篇/1 面试相关/1.2.1 gengerator函数的实战.js
@@ -0,0 +1,29 @@
+
+function* test(i){
+    let a = 5;
+    while(true){
+        i++;
+        if(i>a) break;
+        yield i   //这个i就是每次执行next后返回的参数{value:''}
+
+    }
+}
+
+let t = test(0)
+ t.next()
+// {value: 1, done: false}
+
+ t.next()
+// {value: 2, done: false}
+
+ t.next()
+// {value: 3, done: false}
+
+ t.next()
+// {value: 4, done: false}
+
+ t.next()
+// {value: 5, done: false}
+
+ t.next()
+// {value: undefined, done: true}
