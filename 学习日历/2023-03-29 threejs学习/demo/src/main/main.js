26af4d16a9bc3cef2620d601d0fb8ff6441b40c2
zhurainyk <zhurainyk@163.com>
2023-03-31 17:51:27 +0800

    add

--- 学习日历/2023-03-29 threejs学习/demo/src/main/main.js
+++ 学习日历/2023-03-29 threejs学习/demo/src/main/main.js
@@ -1,3 +1,18 @@
 import * as THREE from 'three'
 console.log(THREE)
-console.log('123')
+
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,2000)
+camera.position.set(0,0,10)
+scene.add(camera)
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color:"#ff00ff"})
+const cube = new THREE.Mesh(geometry,material)
+scene.add(cube)
+const renderer = new THREE.WebGL1Renderer()
+renderer.setSize(window.innerWidth,window.innerHeight)
+
+document.body.appendChild(renderer.domElement)
+
+renderer.render(scene,camera)
---
+++ 学习日历/2023-03-30 threejs初体验/src/main.js
@@ -0,0 +1,128 @@
+import * as THREE from 'three'
+console.log(THREE)
+import { OrbitControls } from './assets/js/OrbitControls'
+
+import gsap from 'gsap'
+//目标 掌握gsap设置各种动画效果  视频地址 https://www.bilibili.com/video/BV1Gg411X7FY?p=13&vd_source=ec3b7218ddc819b2281c36da8b9b6578
+//
+
+
+//1. 创建场景 文档搜索scene
+const scene = new THREE.Scene()
+
+//2. 创建相机  PerspectiveCamera透视相机 这一投影模式被用来模拟人眼所看到的景象，它是3D场景的渲染中使用得最普遍的投影模式。
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000) //参数：角度 宽高比 近 远 
+//摆放相机的位置
+camera.position.set(10, 10, 10)
+//添加相机到场景
+scene.add(camera)
+
+//3. 创建物体
+const geometry = new THREE.BoxGeometry(1, 1, 1) //宽 高 深  
+//基础网格材质(MeshBasicMaterial) 一个以简单着色（平面或线框）方式来绘制几何体的材质。这种材质不受光照的影响。
+const material = new THREE.MeshBasicMaterial({ color: '#ffff00' })
+//合成物体
+const cube = new THREE.Mesh(geometry, material)
+console.log(cube)
+//添加到场景
+scene.add(cube)
+
+//控制物体移动 cube上面有个position的属性 
+// cube.position.set(5,0,0)   //方法
+// cube.position.x = 5 //属性
+
+
+//控制物体缩放 cube上面有个 scale的属性  参数是缩放的倍数  不是长度
+// cube.scale.set(2,1,3) //x2倍 y1倍 z3倍
+cube.scale.x = 1.1
+cube.scale.y = 1.1
+cube.scale.z = 1.1
+
+//物体旋转  cube上面有个rotation的属性
+// cube.rotation.set(Math.PI/4,0,0,'XYZ')
+// cube.rotation.x = Math.PI/4
+
+//创建坐标轴
+const axesHelper = new THREE.AxesHelper(6) //红色x  绿色y  蓝色z
+//坐标轴添加到场景
+scene.add(axesHelper)
+
+//4. 初始化渲染器
+const renderer = new THREE.WebGLRenderer({ antialias: true }) //添加抗锯齿
+//设置渲染的尺寸和大小
+renderer.setSize(window.innerWidth, window.innerHeight)
+//将webgl渲染的canvas内容添加到body
+document.body.appendChild(renderer.domElement)
+//开始渲染
+// renderer.render(scene,camera)
+
+//创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement)
+
+
+//直接使用gsap完成动画效果  官网  docs教程 https://greensock.com/docs/v3/Eases
+// gsap.to(cube.position,{x:10,duration:10,ease: "bounce.out"}) 
+gsap.to(cube.rotation, { y: 2 * Math.PI, duration: 10,delay:5, ease: "elastic.out(1, 0.3)" })
+
+//动画属性配置 
+// gsap.to(cube.position, {
+//     x: 10, duration: 10, ease: "bounce.out", 
+//     onStart() {
+//         console.log('动画开始')
+//     },
+//     onComplete(e){
+//         console.log('动画结束')
+//     },
+//     repeat:2,//-1表示无限循环 
+//     yoyo:true,//往返运动
+// })
+
+//可暂停的动画 控制效果
+const animate1  = gsap.to(cube.position, {
+    x: 10, duration: 3, ease: "bounce.out", 
+    onStart() {
+        console.log('动画开始')
+    },
+    onComplete(e){
+        console.log('动画结束')
+    },
+    delay:0.5,//延迟5s执行动画
+    repeat:3,//-1表示无限循环 
+    yoyo:true,//往返运动
+})
+document.addEventListener('click',()=>{//单击控制动画启停 
+    console.log(animate1)
+    if(animate1.isActive()){//获取动画的运动状态 
+        animate1.pause()
+    }else{
+        animate1.resume()
+    }
+})
+
+
+
+
+//改写开始渲染函数
+
+function render(time) {
+
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(render)
+}
+
+render()
+
+//步骤总结：
+/**
+ * 1.创建场景
+ * 2.创建相机 添加到场景
+ * 3.创建物体 添加到场景
+ * 4.创建渲染器 设置传染尺寸 添加到html 并且执行渲染
+ * 
+ * 创建轨道控制器 需要动画帧渲染  
+ * 
+ * 创建坐标辅助器  添加到场景
+ * 
+ * 移动 旋转 缩放 
+ */
